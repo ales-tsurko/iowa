82eeaf8c80093a25cd5a7833fa6ae751df2e978a
Ales Tsurko <ales.tsurko@gmail.com>
2023-04-07 20:01:28 +0300

    Add number parser

---
+++ iowa-parser/src/lib.rs
@@ -0,0 +1,14 @@
+//! Io programming language using WASM runtime.
+
+#![warn(
+    clippy::all,
+    deprecated_in_future,
+    missing_docs,
+    unused_import_braces,
+    unused_labels,
+    unused_lifetimes,
+    unused_qualifications,
+    unreachable_pub
+)]
+
+pub mod number;
---
+++ iowa-parser/src/number.rs
@@ -0,0 +1,94 @@
+//! Number parser.
+
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{char, one_of},
+    combinator::{map_res, opt, recognize},
+    multi::many1,
+    sequence::{pair, preceded, tuple},
+    IResult,
+};
+
+/// The Number type.
+#[derive(Debug, PartialEq)]
+pub enum Number {
+    /// Hexadecimal number.
+    Hex(u64),
+    /// Decimal number.
+    Decimal(f64),
+}
+
+/// Parse number.
+pub fn number(input: &str) -> IResult<&str, Number> {
+    alt((hex_number, decimal_number))(input)
+}
+
+// Define a parser for hexadecimal digits
+fn hex_number(input: &str) -> IResult<&str, Number> {
+    map_res(
+        preceded(alt((tag("0x"), tag("0X"))), recognize(many1(hex_digit))),
+        |out: &str| u64::from_str_radix(out, 16).map(Number::Hex),
+    )(input)
+}
+
+fn hex_digit(input: &str) -> IResult<&str, char> {
+    one_of("0123456789abcdefABCDEF")(input)
+}
+
+// Define a parser for decimal numbers
+fn decimal_number(input: &str) -> IResult<&str, Number> {
+    let num = recognize(pair(
+        opt(one_of("+-")),
+        alt((
+            // .42
+            recognize(tuple((
+                char('.'),
+                digit,
+                opt(tuple((one_of("eE"), opt(one_of("+-")), digit))),
+            ))), // 42e42 and 42.42e42
+            recognize(tuple((
+                digit,
+                opt(preceded(char('.'), digit)),
+                one_of("eE"),
+                opt(one_of("+-")),
+                digit,
+            ))), // 42. and 42.42
+            recognize(tuple((digit, char('.'), opt(digit)))),
+            // 42
+            recognize(digit),
+        )),
+    ));
+    map_res(num, |out: &str| out.parse::<f64>().map(Number::Decimal))(input)
+}
+
+fn digit(input: &str) -> IResult<&str, &str> {
+    recognize(many1(one_of("0123456789")))(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_hex_number() {
+        assert_eq!(hex_number("0x1234"), Ok(("", Number::Hex(0x1234))));
+        assert_eq!(hex_number("0Xabcd"), Ok(("", Number::Hex(0xABCD))));
+        assert_eq!(hex_number("0x1a2b3c4d"), Ok(("", Number::Hex(0x1A2B3C4D))));
+    }
+
+    #[test]
+    fn test_parse_decimal_number() {
+        assert_eq!(decimal_number("42"), Ok(("", Number::Decimal(42.0))));
+        assert_eq!(decimal_number("3.1415"), Ok(("", Number::Decimal(3.1415))));
+        assert_eq!(
+            decimal_number("123.456e+10"),
+            Ok(("", Number::Decimal(1234560000000.0)))
+        );
+        assert_eq!(decimal_number("0.5e-3"), Ok(("", Number::Decimal(0.0005))));
+        assert_eq!(
+            decimal_number("-2.5e-3"),
+            Ok(("", Number::Decimal(-0.0025)))
+        );
+    }
+}
